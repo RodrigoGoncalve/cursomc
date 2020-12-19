0607de277f54ae4d5c6ccc323fa2f5947524ee9a
Rodrigo Goncalves <ro.goncalve@gmail.com>
2020-12-19 01:41:09 -0300

    Criando Repository Services para Categoria

--- src/main/java/com/rodrigosilva/cursomc/CursomsApplication.java
+++ src/main/java/com/rodrigosilva/cursomc/CursomsApplication.java
@@ -2,7 +2,9 @@ package com.rodrigosilva.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 @SpringBootApplication
 public class CursomsApplication {
 
---
+++ src/main/java/com/rodrigosilva/cursomc/resource/CategoriaResource.java
@@ -0,0 +1,40 @@
+package com.rodrigosilva.cursomc.resource;
+
+//import java.util.ArrayList;
+//import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rodrigosilva.cursomc.domain.Categoria;
+import com.rodrigosilva.cursomc.services.CategoriaService;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value = "/{Id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer Id) {
+		
+		Categoria obj = service.buscar(Id);
+		return ResponseEntity.ok().body(obj);
+		
+		
+		
+		//Categoria cat1 = new Categoria(1, "Informática");
+		//Categoria cat2 = new Categoria(2, "Escritório");
+		
+		//List<Categoria> lista = new ArrayList<>();
+		//lista.add(cat1);
+		//lista.add(cat2);
+		//return lista;
+	}
+
+}
