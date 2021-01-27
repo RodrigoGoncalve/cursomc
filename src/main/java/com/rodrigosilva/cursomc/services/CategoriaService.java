7c92f7c90cbd5ceb305678a6518760ef5b8cc03c
Rodrigo Goncalves <ro.goncalve@gmail.com>
2021-01-27 00:16:37 -0300

    Validação sintatica com Bean Validation

--- src/main/java/com/rodrigosilva/cursomc/services/CategoriaService.java
+++ src/main/java/com/rodrigosilva/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rodrigosilva.cursomc.domain.Categoria;
+import com.rodrigosilva.cursomc.dto.CategoriaDTO;
 import com.rodrigosilva.cursomc.repositories.CategoriaRepository;
 import com.rodrigosilva.cursomc.services.exception.ObjectNotFoundException;
 
@@ -50,5 +51,8 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }	
