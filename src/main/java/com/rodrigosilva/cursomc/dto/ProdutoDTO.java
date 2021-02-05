318b7362b4e8979cf956eea88eb0064ee1ec2b28
Rodrigo Goncalves <ro.goncalve@gmail.com>
2021-02-05 18:19:42 -0300

    busca de produtos por nome e categorias

---
+++ src/main/java/com/rodrigosilva/cursomc/dto/ProdutoDTO.java
@@ -0,0 +1,49 @@
+package com.rodrigosilva.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.rodrigosilva.cursomc.domain.Produto;
+
+public class ProdutoDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id ;
+	private String nome ;
+	private Double price ;
+	
+	public ProdutoDTO() {
+		
+	}
+	public ProdutoDTO(Produto obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		price = obj.getPrice();
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+
+}
--- src/main/java/com/rodrigosilva/cursomc/resource/PedidoResourse.java
+++ src/main/java/com/rodrigosilva/cursomc/resource/PedidoResourse.java
@@ -13,15 +13,16 @@ import com.rodrigosilva.cursomc.services.PedidoService;
 @RestController
 @RequestMapping(value = "/pedidos")
 public class PedidoResourse {
-	
+
 	@Autowired
 	private PedidoService service;
-	
-	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
-		
+
 	}
+	
 
 }
