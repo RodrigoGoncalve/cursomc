30c151f1a76a76f6808cb7566f791f3b0c9f5667
Rodrigo Goncalves <ro.goncalve@gmail.com>
2021-02-17 17:03:06 -0300

    envio do projeto para produção

--- src/main/java/com/rodrigosilva/cursomc/CursomsApplication.java
+++ src/main/java/com/rodrigosilva/cursomc/CursomsApplication.java
@@ -1,19 +1,25 @@
 package com.rodrigosilva.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rodrigosilva.cursomc.services.DBService;
+
 @SpringBootApplication
 public class CursomsApplication implements CommandLineRunner {
-
+	
+	@Autowired
+	private DBService dbservice;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomsApplication.class, args);
+		
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		
+		dbservice.instantiateTestDatabase();
 	}
 }
